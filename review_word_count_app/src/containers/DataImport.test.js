dad4a8c41706f14072b8505bb268fd712623f9f3
Adams1991 <adams121@hotmail.co.uk>
2018-09-06 09:51:29 +0100

    passes tests showing data being imported

---
+++ review_word_count_app/src/containers/DataImport.test.js
@@ -0,0 +1,12 @@
+import DataImport from '../../../DataImport';
+const assert = require('assert');
+
+it('can import data array from outside of app', () => {
+  const result = DataImport.length;
+  assert.deepStrictEqual(result, 20);
+});
+
+it('is getting correct data from import', () => {
+  const result = DataImport[0];
+  assert.deepStrictEqual(result, "A great product for daily use,");
+})
