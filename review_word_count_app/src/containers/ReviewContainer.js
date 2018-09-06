1117321a0577fd9cb678d20dca9d223a6609970b
Adams1991 <adams121@hotmail.co.uk>
2018-09-06 10:36:12 +0100

    ReviewContainer render test passing

--- review_word_count_app/src/containers/ReviewContainer.js
+++ review_word_count_app/src/containers/ReviewContainer.js
@@ -1,3 +1,27 @@
+import React, { Component } from 'react';
+import './ReviewContainer.css';
+
+class ReviewContainer extends Component {
+  render() {
+    return (
+      <div className="ReviewContainer">
+
+      </div>
+    );
+  }
+}
+
+export default ReviewContainer;
+
+
+
+
+
+
+
+
+
+
 // TODO: Import Data, Set state to imported data
 // TODO: Create Method to get word count from array (using hash)
 // TODO: Pass down hash as prop
