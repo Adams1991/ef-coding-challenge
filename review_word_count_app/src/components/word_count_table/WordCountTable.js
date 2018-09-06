2d4a7abb3e108a2dfe937c922720c48bcc3b212d
Adams1991 <adams121@hotmail.co.uk>
2018-09-06 12:33:54 +0100

    renders blank table

---
+++ review_word_count_app/src/components/word_count_table/WordCountTable.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class WordCountTable extends React.Component {
+  constructor(props) {
+      super(props);
+  }
+
+  render() {
+    return(
+      <table>
+
+      </table>
+    )
+  }
+
+
+
+}
+
+export default WordCountTable;
