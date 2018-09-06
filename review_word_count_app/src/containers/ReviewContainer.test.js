ac0afee3ed6278b7deb2bab691badb1ed576e339
Adams1991 <adams121@hotmail.co.uk>
2018-09-06 10:34:39 +0100

    adds first test for render of container, failing as no ReviewContainer react element

--- review_word_count_app/src/containers/ReviewContainer.test.js
+++ review_word_count_app/src/containers/ReviewContainer.test.js
@@ -1,2 +1,14 @@
-// TODO: Import assert, ReactDOM etc.
+import DataImport from '../../../DataImport';
+const assert = require('assert');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReviewContainer from './ReviewContainer';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<ReviewContainer />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
 // TODO: create test checking hash correctly mapped, render working etc.
